43e8d8ee1c96e4db43febb98a7d2a904ed616fae
Curtinhas <carlos_curtinhas@sapo.pt>
2024-12-12 18:21:18 +0000

    handling post request

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const fs = require('fs');
-//const path = require('path');
+
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+// Carregar os dados iniciais
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
+// Rota GET para obter todos os tours
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -16,6 +20,28 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// Rota POST para adicionar um novo tour
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.length > 0 ? tours[tours.length - 1].id + 1 : 1; // Gerar novo ID
+  const newTour = { id: newId, ...req.body }; // Criar novo tour com ID e dados recebidos
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}...`);
 });
